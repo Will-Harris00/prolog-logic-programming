ba62a45e9e22ba985bdcabf32e5a655ef088eebc
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 21:06:34 +0100

    Assess whether number is a par

--- Task 2 - Prolog/par.pl
+++ Task 2 - Prolog/par.pl
@@ -1,13 +1,29 @@
 :- initialization(main).
-get_num(P,Q,R) :-
-    R is P * 10 + Q.
+convert(P, Q) :-
+    Q is P - 48,
+    Q > 0.
+
+unique(List) :-
+    sort(List, Sorted),
+    length(List, OriginalLength),
+    length(Sorted, SortedLength),
+    OriginalLength =:= SortedLength.
 
 multiple_of([A,B,C,D]) :-
     get_num(A,B,X),
     get_num(C,D,Y),
     X mod Y =:= 0.
-    %X mod Y is 0.
+
+get_num(P,Q,R) :-
+    R is P * 10 + Q.
+
+par(Number) :-
+    number_codes(Number, ListCharCodes), 
+    maplist(convert, ListCharCodes, ListDigits),
+    length(ListDigits, 4),
+    unique(ListDigits),
+    multiple_of(ListDigits).
 
 
 main :-
-    multiple_of( [7, 8, 2, 6] ).
+    par( 7826 ).
