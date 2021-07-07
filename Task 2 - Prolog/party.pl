484198bce963c6da68dae0fdaf83aeb2710adedc
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 21:13:46 +0100

    Find missing digit

---
+++ Task 2 - Prolog/party.pl
@@ -0,0 +1,18 @@
+:- initialization(main).
+convert(P, Q) :-
+    Q is P - 48,
+    Q > 0.
+
+missing_digit(V,W,D) :-
+    number_codes(V, ListOne),
+    number_codes(W, ListTwo),
+    maplist(convert, ListOne, ListDigitsOne),
+    maplist(convert, ListTwo, ListDigitsTwo),
+    append(ListDigitsOne, ListDigitsTwo, D).
+
+party(V, W) :-
+    missing_digit(V, W, D), write(D).
+
+
+main :-
+    party( 9632, 5418 ).
