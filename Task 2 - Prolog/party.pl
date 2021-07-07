69afdfc9bcdee936d8c391f6121ac10475e8957f
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 21:16:35 +0100

    Check only one number is missing

--- Task 2 - Prolog/party.pl
+++ Task 2 - Prolog/party.pl
@@ -3,16 +3,17 @@ convert(P, Q) :-
     Q is P - 48,
     Q > 0.
 
-missing_digit(V,W,D) :-
+missing_digit(V,W,CombDigits) :-
     number_codes(V, ListOne),
     number_codes(W, ListTwo),
     maplist(convert, ListOne, ListDigitsOne),
     maplist(convert, ListTwo, ListDigitsTwo),
-    append(ListDigitsOne, ListDigitsTwo, D).
+    append(ListDigitsOne, ListDigitsTwo, CombDigits).
 
 party(V, W) :-
-    missing_digit(V, W, D),
-    subtract([1,2,3,4,5,6,7,8,9], D, M), write(M).
+    missing_digit(V, W, CombDigits),
+    subtract([1,2,3,4,5,6,7,8,9], CombDigits, MissingDigits),
+    length(MissingDigits, 1).
 
 
 main :-
