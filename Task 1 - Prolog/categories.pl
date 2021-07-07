6954e38bee06c1fcbbbfb133bc61ba322fc8314e
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 19:44:36 +0100

    Simplified category definitions

--- Task 1 - Prolog/categories.pl
+++ Task 1 - Prolog/categories.pl
@@ -1,35 +1,13 @@
-animal( aardvark ).
-animal( antelope ).
-animal( coyote ).
-animal( dingo ).
-animal( donkey ).
-animal( elephant ).
-animal( horse ).
-animal( jaguar ).
-animal( kangaroo ).
+:- initialization(main).
+animal(C) :-
+    member(C, [aardvark, antelope, coyote, dingo, donkey, elephant, horse, jaguar, kangaroo]).
 
-vegetable( artichoke ).
-vegetable( cabbage ).
-vegetable( carrot ).
-vegetable( celery ).
-vegetable( leek ).
-vegetable( lettuce ).
-vegetable( marrow ).
-vegetable( onion ).
-vegetable( potato ).
+vegetable(C) :-
+    member(C, [artichoke, cabbage, carrot, celery, leek, lettuce, marrow, onion, potato]).
 
-mineral( anatase ).
-mineral( basalt ).
-mineral( cobolt ).
-mineral( copper ).
-mineral( galena ).
-mineral( nickel ).
-mineral( sodium ).
-mineral( silver ).
-mineral( zicron ).
+mineral(C) :-
+    member(C, [anatase, basalt, cobolt, copper, galena, nickel, sodium, silver, zicron]).
 
-/* queries
-animal( kangaroo ).
-vegetable( onion ).
-mineral( sodium ).
-*/
+
+main :-
+    animal( aardvark ), write(true).
