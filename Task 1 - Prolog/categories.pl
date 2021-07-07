60fa78af1d2b6a2e879609037c72044855e9c99b
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 19:43:51 +0100

    Defined categories of entities

---
+++ Task 1 - Prolog/categories.pl
@@ -0,0 +1,35 @@
+animal( aardvark ).
+animal( antelope ).
+animal( coyote ).
+animal( dingo ).
+animal( donkey ).
+animal( elephant ).
+animal( horse ).
+animal( jaguar ).
+animal( kangaroo ).
+
+vegetable( artichoke ).
+vegetable( cabbage ).
+vegetable( carrot ).
+vegetable( celery ).
+vegetable( leek ).
+vegetable( lettuce ).
+vegetable( marrow ).
+vegetable( onion ).
+vegetable( potato ).
+
+mineral( anatase ).
+mineral( basalt ).
+mineral( cobolt ).
+mineral( copper ).
+mineral( galena ).
+mineral( nickel ).
+mineral( sodium ).
+mineral( silver ).
+mineral( zicron ).
+
+/* queries
+animal( kangaroo ).
+vegetable( onion ).
+mineral( sodium ).
+*/
