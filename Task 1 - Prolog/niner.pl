65a0b6eb7ec77da91b26404bd87c83a0bc89fc3d
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 20:01:59 +0100

    Simplified teaser output

--- Task 1 - Prolog/niner.pl
+++ Task 1 - Prolog/niner.pl
@@ -14,13 +14,13 @@ spell(S, X) :-
 niner(_) :-
     animal(A),
     spell(A, [T4, T5, T2, T5, T8, T9]),
-    write(T4), write(T5), write(T2), write(T5), write(T8), write(T9), write('\n'),
+    maplist(write, [T4, T5, T2, T5, T8, T9, '\n']),
     vegetable(V),
     spell(V, [T6, T5, T8, T7, T8, T5]),
-    write(T6), write(T5), write(T8), write(T7), write(T8), write(T5),  write('\n'),
+    maplist(write, [T6, T5, T8, T7, T8, T5, '\n']),
     mineral(M),
     spell(M, [T7, T3, T7, T8, T7, T1, T9]),
-    write(T7), write(T3), write(T7), write(T8), write(T7), write(T1), write(T9).
+    maplist(write, [T7, T3, T7, T8, T7, T1, T9]).
 
 /*
 a( 452589 ).
