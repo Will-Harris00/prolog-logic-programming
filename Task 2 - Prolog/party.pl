2390775d304620ae4f120849512e976b41fb65db
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 21:21:25 +0100

    Find whether two pars contain completely unique digits

--- Task 2 - Prolog/party.pl
+++ Task 2 - Prolog/party.pl
@@ -3,6 +3,28 @@ convert(P, Q) :-
     Q is P - 48,
     Q > 0.
 
+unique(List) :-
+    sort(List, Sorted),
+    length(List, OriginalLength),
+    length(Sorted, SortedLength),
+    OriginalLength =:= SortedLength.
+
+multiple_of([A,B,C,D]) :-
+    get_num(A,B,X),
+    get_num(C,D,Y),
+    X mod Y =:= 0.
+    
+get_num(P,Q,R) :-
+    R is P * 10 + Q.
+
+par(Number) :-
+    number_codes(Number,ListCharCodes), 
+    maplist(convert,ListCharCodes,ListDigits),
+    length(ListDigits, 4),
+    unique(ListDigits),
+    multiple_of(ListDigits).
+
+
 missing_digit(V,W,CombDigits) :-
     number_codes(V, ListOne),
     number_codes(W, ListTwo),
@@ -13,7 +35,9 @@ missing_digit(V,W,CombDigits) :-
 party(V, W) :-
     missing_digit(V, W, CombDigits),
     subtract([1,2,3,4,5,6,7,8,9], CombDigits, MissingDigits),
-    length(MissingDigits, 1).
+    length(MissingDigits, 1),
+    par(V),
+    par(W).
 
 
 main :-
