69c6c55356b9ae097946e4a1f7ab27e22480a623
Will-Harris00 <wjharris00@outlook.com>
2021-07-07 19:53:48 +0100

    Template for animal category mapping

---
+++ Task 1 - Prolog/niner.pl
@@ -0,0 +1,26 @@
+:- initialization(main).
+animal(C) :-
+    member(C, [aardvark, antelope, coyote, dingo, donkey, elephant, horse, jaguar, kangaroo]).
+
+vegetable(C) :-
+    member(C, [artichoke, cabbage, carrot, celery, leek, lettuce, marrow, onion, potato]).
+
+mineral(C) :-
+    member(C, [anatase, basalt, cobolt, copper, galena, nickel, sodium, silver, zicron]).
+
+spell(S, X) :-
+    atom_chars(S, X).
+
+niner(_) :-
+    animal(coyote),
+    spell(coyote, [T4, T5, T2, T5, T8, T9]), write(T9), write(T8), write(T5), write(T2), write(T5), write(T4).
+
+/*
+a( 452589 ).
+v( 658785 ).
+m( 7378719 ).
+*/
+
+
+main :- 
+    niner(_).
